4ea2488e3438bf215d68b535d1bc9a2c3ce7356a
jasmer-phpdevloper <138756604+jasmer-phpdevloper@users.noreply.github.com>
2023-07-06 16:01:27 +0530

    inital commit

--- resources/views/Therapist/dashboard.blade.php
+++ resources/views/Therapist/dashboard.blade.php
@@ -554,8 +554,7 @@
           </div>
         </div> -->
       </div>
-      <!-- content-wrapper ends -->
-      <!-- partial:partials/_footer.html -->
+    
       <footer class="footer">
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
           <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © protolabzit.com 2023</span>
