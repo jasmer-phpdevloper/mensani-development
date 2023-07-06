7d45dbb5bca980ba875559f24a80934716954804
jasmer-phpdevloper <138756604+jasmer-phpdevloper@users.noreply.github.com>
2023-07-06 15:38:34 +0530

    appointment listing on dashboard

--- resources/views/Therapist/dashboard.blade.php
+++ resources/views/Therapist/dashboard.blade.php
@@ -4,6 +4,14 @@
  .time-btn p {
     text-transform: capitalize;
 }
+
+ .sidebar {
+  overflow-y: scroll;
+    width: 20% !important;
+    height: 120%;
+  }
+
+
 </style>
 @section('main_section')
 <meta name="csrf-token" content="{{ csrf_token() }}" />
@@ -432,8 +440,51 @@
             </div>
           </div>
         </div> --}}
-       
         <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">Today appointments</h4>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="table-responsive">
+                      <table class="table">
+                        <thead>
+                        <tr>
+                            <th style="color:white"><b>Athelete</b></th>
+                            <th style="color:white"><b>Start Time</b></th>
+                            <th style="color:white"><b>End  Time</b></th>
+                         
+                          </tr>
+                        </thead>  
+                        <tbody>
+                          @php   $events = App\Models\Booking::with('Athlete')->where('therapist_id',\Auth::guard('therapist')->user()->id)->whereDate('date', date('Y-m-d'))
+            ->get();
+                    
+                         @endphp
+                         @foreach ($events as $event)
+                          <tr>
+                            <td>
+                             {{$event->athlete['name']}}
+                            </td>
+                            <td>  {{$event->start_time}}</td>
+                            <td>  {{$event->end_time}}</td>
+                          </tr>
+                         @endforeach
+                          
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                  <!-- <div class="col-md-7">
+                    <div id="audience-map" class="vector-map"></div>
+                  </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="row">
           <div class="col-12">
             <div class="card">
               <div class="card-body">
@@ -502,7 +553,7 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
       </div>
       <!-- content-wrapper ends -->
       <!-- partial:partials/_footer.html -->
